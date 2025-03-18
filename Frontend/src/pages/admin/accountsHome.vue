e050afdd966b241ce1a8c820bc2613e7b055b29f
qmarkm <106965891+qmarkm@users.noreply.github.com>
2025-03-18 22:03:29 +0800

    Improved sub category files
    Included routing of nested routes

--- Frontend/src/pages/admin/accountsHome.vue
+++ Frontend/src/pages/admin/accountsHome.vue
@@ -2,17 +2,20 @@
     import { useSetValidationUser } from '../../Queries';
     import {useGetUnverifiedUsers} from '../../Queries';
     import HeaderX from "../../components/HeaderX.vue";
+    import { useRouter } from "vue-router";
     
     const {data: unverifiedUsersData, isLoading: unverifiedUsersIsLoading , isError: unverifiedUsersIsError, error: unverifiedUsersError } = useGetUnverifiedUsers();
-
+    const router = useRouter()
 
     const {useSetValidationUserAsync} = useSetValidationUser(); 
 
     const updateUserValidation = async (userId, validationStatus) => {
         await useSetValidationUserAsync({userId, validationStatus});
     }
-
-    const router = useRouter()
+    
+    function switchTo(url){
+        router.replace(url)
+    }
 </script>
 
 <template>
@@ -22,14 +25,14 @@
         <!--Navigation button divs-->
         <div name="subcategories" class="bg-blue-300 w-fit h-fit rounded-b-xl p-2 flex flex-row">
 
-            <div name="button1" class="items-center flex flex-col mx-2">
+            <div name="button1" class="items-center flex flex-col mx-2" @click="switchTo('/admin/accounts/viewAccounts')">
                 <button name="c1" class="rounded-full bg-white mx-4 p-4 w-fit">
                     <v-icon name="co-user" scale="2.5" fill="#000000" animation="float" hover/>
                 </button>
                 <span class="text-center">View Users</span>
             </div>
 
-            <div name="button2" class="items-center flex flex-col mx-2">
+            <div name="button2" class="items-center flex flex-col mx-2" @click="switchTo('/admin/accounts/approveAccounts')">
                 <button name="c2" class="rounded-full bg-white mx-4 p-4 w-fit">
                     <v-icon name="fa-user-check" scale="2.5" fill="#000000" animation="float" hover/>
                 </button>
@@ -37,9 +40,9 @@
             </div>
 
             <div name="button3" class="items-center flex flex-col mx-2">
-                <button name="c3" class="rounded-full bg-white mx-4 p-4 w-fit" onclick="console.log('test');">
+                <div name="c3" class="rounded-full bg-white mx-4 p-4 w-fit" @click="switchTo('/admin/accounts/companies')">
                     <v-icon name="co-building" scale="2.5" fill="#000000" animation="float" hover/>
-                </button>
+                </div>
                 <span class="text-center">View Companies</span>
             </div>
 
--- Frontend/src/pages/admin/userList.vue
+++ Frontend/src/pages/admin/userList.vue
@@ -3,5 +3,32 @@
 </script>
 
 <template>
-    View Users here!!!
+    <div class="py-8">
+        <div name="filters">
+            <button class="mx-2">All</button>
+            <button class="mx-2">Admins</button>
+            <button class="mx-2">Clients</button>
+            <button class="mx-2">Couriers</button>
+        </div>
+
+        <table class="w-full text-sm text-left rtl:text-right table-auto border border-gray-300">
+            <thead class="border-b border-gray-500">
+                <tr>
+                    <th scope="col" class="px-6 py-3 border border-gray-500">Username</th>
+                    <th scope="col" class="px-6 py-3 border border-gray-500">User Type</th>
+                    <th scope="col" class="px-6 py-3 border border-gray-500">Company</th>
+                    <th scope="col" class="px-6 py-3 border border-gray-500">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="border-b border-gray-300">
+                    <td class="px-6 py-3 border border-gray-500">Test</td>
+                    <td class="px-6 py-3 border border-gray-500">Admin</td>
+                    <td class="px-6 py-3 border border-gray-500">DLSU</td>
+                    <td class="px-6 py-3 border border-gray-500">practicum.it@dlsu.edu.ph</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
 </template>
