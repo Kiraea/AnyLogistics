0d8bdfaf85c49078af62577820998fe3bc588ae5
Kiraea <joshuavillavieja@gmail.com>
2025-04-09 14:19:27 +0800

    added create vehicles

--- Backend/routes/vehicle.js
+++ Backend/routes/vehicle.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { queries } from '../queries.js';
 
 import { pool } from '../index.js';
+import { verifySessionToken } from '../middlewares/sessionUtils.js';
 let router = express.Router()
 
 
@@ -19,6 +20,33 @@ router.get(`/`, async (req, res)=> {
     }    
 }) 
 
+router.post(`/`, verifySessionToken, async (req, res)=> {
+    const {userId} = req
+    const {vehicleType, cityId} = req.body
+
+    let  maxCapacityKg;
+    if (vehicleType){
+        if (vehicleType === "light"){
+            maxCapacityKg = 200
+        }else if(vehicleType === "medium"){
+            maxCapacityKg = 500
+        }else{
+            maxCapacityKg = 1000
+        }
+    }
+
+    try{
+        let result = await pool.query(queries.vehicle.addNewVehicle, [userId, vehicleType, maxCapacityKg, cityId]);
+        if (result.rowCount > 0){
+            res.status(200).json({status: "success", message: "succesfully get vehicle", data: result.rows})
+        }else{
+            res.status(200).json({status: "success", message: "no vehicle found", data: null})
+        }
+    }catch(e){
+        console.log(e)
+        res.status(500).json({status: "error", message: "Cannot get vehicle " })
+    }    
+}) 
 
 
 
